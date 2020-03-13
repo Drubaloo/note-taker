5330cd60c78217565ca226ba590bd3b1689b37a8
Drubaloo <Drubaloo@outlook.com>
2020-03-13 16:18:16 -0700

    Solved the MIME errors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var path = require("path");
 const fs = require(`fs`)
-const notes = require(`../../../db/db.json`)
+const notes = require(`./db/db.json`)
 
 // Sets up the Express App
 // =============================================================
@@ -13,25 +13,33 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'))
 
 //(DATA)
 // =============================================================
-
-
-// Routes
+ 
+// Routes 
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/public/assets/css/styles.css", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
+})
+
+app.get("/public/assets/js/index.js", function (req, res){
+    res.sendFile(path.join(__dirname, "./public/assets/js/index.js"))
+})
+
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile('../../../db/db.json', `utf-8`, (err, data) => {
+    fs.readFile('./db/db.json', `utf-8`, (err, data) => {
         if (err) throw err;
         return res.json(notes)
         
@@ -43,7 +51,7 @@ app.post(`/api/notes`, function (req, res) {
     newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase()
     newNote.id = newNote.title.replace(/\s+/g, "").toUpperCase()
     
-    console.log(`receiving data`)
+    console.log(`receiving data`) 
     console.log(`body is `, req.body)
 
     notes.push(newNote)
@@ -56,7 +64,7 @@ app.delete(`/api/notes/:id`, function (req, res){
     console.log(delNote)
     for (i = 0; i < notes.length; i++)
     if (delNote == notes[i].id){
-        notes.pop(notes[i])
+        notes.pop(notes[i])  //NEED TO FIND ALTERNATIVE
         
     } 
 })
